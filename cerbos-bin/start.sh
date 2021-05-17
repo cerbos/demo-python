bf4a4059730868b7199745e1a2130a130d3e2656
emreb <emre@cerbos.dev>
2021-05-17 22:20:11 +0100

    Config file pickup
    Changing config so local config file is read and applied.

--- cerbos-bin/start.sh
+++ cerbos-bin/start.sh
@@ -6,7 +6,10 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONTAINER_IMG=${CONTAINER_IMG:-"pkg.cerbos.dev/containers/cerbos"}
 CONTAINER_TAG=${CONTAINER_TAG:-"0.0.1-rc2"}
 
-docker run -i -t -p 3592:3592 \
-  -v ${SCRIPT_DIR}/policy:/policies \
-  "${CONTAINER_IMG}:${CONTAINER_TAG}"
+docker run -i -t -p 9998:9998 \
+  -v ${SCRIPT_DIR}/policy:/policy \
+  -v ${SCRIPT_DIR}/config:/config \
+  "${CONTAINER_IMG}:${CONTAINER_TAG}" \
+  server --config=/config/conf.yaml
+
 
