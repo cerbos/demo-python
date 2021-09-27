1ac6cd8d5a6e1c063d9e001da6e8756d7f38631a
emreb <emre@cerbos.dev>
2021-09-27 22:13:19 +0100

    Cerbos binary version update (#15)
    Signed-off-by: emreb <emre@cerbos.dev>

--- cerbos-bin/start.sh
+++ cerbos-bin/start.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONTAINER_IMG=${CONTAINER_IMG:-"ghcr.io/cerbos/cerbos"}
 
-CONTAINER_TAG=${CONTAINER_TAG:-"0.4.0"}
+CONTAINER_TAG=${CONTAINER_TAG:-"0.5.0"}
 
 docker run -i -t -p 3592:3592 \
   -v ${SCRIPT_DIR}/policy:/policies \
