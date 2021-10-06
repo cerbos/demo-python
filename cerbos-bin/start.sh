8341a0ff2ced0d1c7fa112937db0dd23e9ecc373
emreb <emre@cerbos.dev>
2021-10-06 13:38:56 +0100

    Cerbos version bump to v8 (#17)
    Signed-off-by: emreb <emre@cerbos.dev>

--- cerbos-bin/start.sh
+++ cerbos-bin/start.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONTAINER_IMG=${CONTAINER_IMG:-"ghcr.io/cerbos/cerbos"}
 
-CONTAINER_TAG=${CONTAINER_TAG:-"0.5.0"}
+CONTAINER_TAG=${CONTAINER_TAG:-"0.8.0"}
 
 docker run -i -t -p 3592:3592 \
   -v ${SCRIPT_DIR}/policy:/policies \
