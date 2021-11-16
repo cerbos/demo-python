0eb0e5d294e788e170a2e2915bbe7781f14d96db
Oğuzhan D <oguzhandurgun95@gmail.com>
2021-11-16 22:21:14 +0300

    chore: Retrieve latest cerbos version from github releases
    Signed-off-by: Oğuzhan D <oguzhandurgun95@gmail.com>

--- cerbos-bin/start.sh
+++ cerbos-bin/start.sh
@@ -2,13 +2,12 @@
 
 set -euo pipefail
 
+LATEST_VERSION=$(curl --silent "https://api.github.com/repos/cerbos/cerbos/releases/latest" | jq -r .tag_name)
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONTAINER_IMG=${CONTAINER_IMG:-"ghcr.io/cerbos/cerbos"}
 
-CONTAINER_TAG=${CONTAINER_TAG:-"0.8.0"}
+CONTAINER_TAG=${CONTAINER_TAG:-"${LATEST_VERSION:1}"}
 
 docker run -i -t -p 3592:3592 \
   -v ${SCRIPT_DIR}/policy:/policies \
   "${CONTAINER_IMG}:${CONTAINER_TAG}"
-
-
